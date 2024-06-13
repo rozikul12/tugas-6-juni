3e469cf114b9984e3bd2eb9affbb7c16101a3bdf
rozikul12 <164202720+rozikul12@users.noreply.github.com>
2024-06-13 17:34:15 +0700

    Create formlogin.php

---
+++ formlogin.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Login App</h1>
+    <form action="login.php" method="post">
+        <table border="0">
+            <tr>
+                <td>NIM</td>
+                <td>:</td>
+                <td><input type="text" name="NIM"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td>:</td>
+                <td><input type="Password" name="Password"></td>
+            </tr>
+        </table><br>
+        <input type="submit" value="kirim">
+</body>
+</html>
